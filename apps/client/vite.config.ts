aec3a02fdf51101724ece597a1653d825909d5e8
Nicolas Lanore <nicolas.lanore@gmail.com>
2025-02-28 23:12:03 +0100

    Added VitePWA plugin

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import * as path from "path";
+import { defineConfig, loadEnv } from "vite";
+import { VitePWA } from 'vite-plugin-pwa';
 
 export const envPath = path.resolve(process.cwd(), "..", "..");
 
@@ -21,7 +22,33 @@ export default defineConfig(({ mode }) => {
       },
       APP_VERSION: JSON.stringify(process.env.npm_package_version),
     },
-    plugins: [react()],
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        workbox: {
+          maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // Increase the limit to 5MB (or your desired size)
+        },
+        manifest: {
+          name: 'Docmost',
+          short_name: 'Docmost',
+          description: 'Open-source collaborative wiki and documentation software',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: 'pwa-icon-192.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: 'pwa-icon-512.png',
+              sizes: '512x512',
+              type: 'image/png'
+            }
+          ]
+        },
+      }),
+    ],
     resolve: {
       alias: {
         "@": "/src",
